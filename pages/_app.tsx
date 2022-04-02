aab5a6ec08a0844d488277fb008b491ce48a4b4a
Tsvetkov <finnerofficial@gmail.com>
2022-04-03 00:36:52 +0300

    Add layout for app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,28 @@ import '../styles/globals.css'
 import type {AppProps} from 'next/app'
 import {SessionProvider} from 'next-auth/react'
 import {RecoilRoot} from "recoil";
+import SideBar from "../components/SideBar";
+import Player from "../components/Player";
+import React from "react";
+import AccountButton from "../components/AccountButton";
 
 function MyApp({Component, pageProps: {session, ...pageProps}}: AppProps) {
     return (
         <SessionProvider session={session}>
             <RecoilRoot>
-                <Component {...pageProps} />
+                <div className={"bg-black h-screen overflow-hidden"}>
+                    <AccountButton/>
+                    <main className={"flex"}>
+                        <SideBar/>
+                        <Component {...pageProps} />
+                    </main>
+                    <div className={"absolute w-screen bottom-0"}>
+                        <Player/>
+                    </div>
+                </div>
             </RecoilRoot>
         </SessionProvider>
     )
 }
 
-export default MyApp
+export default MyApp;
