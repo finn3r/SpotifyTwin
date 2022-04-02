fdb5221e6d9fcbb273481a295ea7e722fc2fe670
Tsvetkov <finnerofficial@gmail.com>
2022-04-03 00:30:10 +0300

    Add 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Custom404 = () => {
+    return (
+        <div className={"h-screen w-screen flex justify-center items-center"}>
+            <h1 className={"text-white text-2xl md:text-3xl xl:text-5xl font-bold"}>
+                Page not found.
+            </h1>
+        </div>
+    );
+};
+
+export default Custom404;
