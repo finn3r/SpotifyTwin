e16f458f8efb47bc825a7fb968977af2149435e1
finner <finnerofficial@gmail.com>
2022-04-12 19:55:51 +0300

    Fixed sidebar showing current page

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -43,7 +43,7 @@ const SideBar = () => {
 
     return (
         <div
-            className="hidden sm:block text-gray-500 p-5 text-xs border-r border-gray-900 h-screen overflow-y-scroll scrollbar-hide lg:text-sm sm:max-w-[12rem] lg:max-w-[15rem] pb-36">
+            className="hidden sm:block text-gray-500 p-5 text-xs border-r border-gray-900 lg:text-sm sm:max-w-[12rem] lg:max-w-[15rem] pb-36">
             <div className={"space-y-4"}>
                 {/*Buttons*/}
                 <button className={`flex items-center space-x-2 hover:text-white ${("/" == path) ? "text-white" : ""}`} onClick={() => router.push(`/`)}>
@@ -54,7 +54,7 @@ const SideBar = () => {
                     <SearchIcon className="h-5 w-5"/>
                     <p>Search</p>
                 </button>
-                <button className={`flex items-center space-x-2 hover:text-white ${(path.includes("/collection")&&!path.includes("/tracks")) ? "text-white" : ""}`} onClick={() => router.push(`/collection/playlists`)}>
+                <button className={`flex items-center space-x-2 hover:text-white ${(path.includes("/collection")&&!path.includes("/tracks")&&!path.includes("/episodes")) ? "text-white" : ""}`} onClick={() => router.push(`/collection/playlists`)}>
                     <LibraryIcon className="h-5 w-5"/>
                     <p>Your Library</p>
                 </button>
@@ -74,15 +74,17 @@ const SideBar = () => {
                 </button>
                 <hr className={"border-t-[0.1px] border-gray-900"}/>
                 {/*Playlists*/}
+                <div className={"h-[70vh] overflow-y-scroll scrollbar-hide pb-20"}>
                 {playlists.map((playlist) => (
                     <p
                         key={playlist.id}
-                        className={`cursor-pointer hover:text-white ${(playlistId == playlist.id) ? "text-white" : ""}`}
+                        className={`pb-4 cursor-pointer hover:text-white ${(playlistId == playlist.id) ? "text-white" : ""}`}
                         onClick={() => router.push(`/playlist/${playlist.id}`)}
                     >
                         {playlist.name}
                     </p>
                 ))}
+                </div>
             </div>
         </div>
     )
