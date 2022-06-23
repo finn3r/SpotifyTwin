10cdd313130d32b5003dc75481e4dcf958a021f6
Tsvetkov Ivan <finnerofficial@gmail.com>
2022-06-23 21:19:51 +0300

    Fixed search input

--- pages/search.tsx
+++ pages/search.tsx
@@ -10,6 +10,7 @@ const Search = () => {
     const [, setTitle] = useRecoilState(titleAtom);
     const spotifyApi = useSpotify();
     const [value, setValue] = useState("");
+    const [isFetching, setIsFetching] = useState(false);
     const [searchResults, setSearchResults] = useState<SpotifyApi.SearchResponse>();
     const debouncedValue = useDebounce<string>(value, 250);
     const cellMinWidth: number = 250;
@@ -28,20 +29,24 @@ const Search = () => {
 
     const SearchRequest = async () => {
         try {
+            setIsFetching(true);
             const results = await spotifyApi.search(debouncedValue, ["artist", "track", "album"], {limit: 10}).then(data => data.body);
-            console.log(results);
             setSearchResults(results);
         } catch (e) {
             console.log(e);
+        } finally {
+            setIsFetching(false);
         }
     };
 
     useEffect(() => {
-        setTitle(`Search${debouncedValue ? `:${debouncedValue}` : ""} - Spotify tween`);
-    }, [debouncedValue]);
-
-    useEffect(() => {
-        if (debouncedValue) SearchRequest().then();
+        if (debouncedValue) {
+            setTitle(`Search:${debouncedValue} - Spotify tween`);
+            SearchRequest().then();
+        } else {
+            setTitle("Search - Spotify tween")
+            setSearchResults(undefined);
+        }
     }, [debouncedValue]);
 
     return (
@@ -54,26 +59,32 @@ const Search = () => {
                        className={"p-5 w-[calc(100%-13rem)] h-10 rounded-full bg-white border-gray-300 border-2 outline-none"}
                 />
             </div>
-            {((searchResults?.tracks?.total!==0||searchResults?.artists?.total!==0||searchResults?.albums?.total!==0)&&searchResults) ?
+            {((searchResults?.tracks?.total !== 0 || searchResults?.artists?.total !== 0 || searchResults?.albums?.total !== 0) && searchResults) ?
                 <div className={"pb-36 h-screen scrollbar-hide overflow-y-scroll"}>
-                    <p className={"flex flex-row space-x-4 text-white text-xl md:text-2xl xl:text-3xl font-bold m-5"}>{searchResults.tracks?.items.length!==0 ? "Tracks" : "Tracks not found"}</p>
+                    <p className={"flex flex-row space-x-4 text-white text-xl md:text-2xl xl:text-3xl font-bold m-5"}>{searchResults.tracks?.items.length !== 0 ? "Tracks" : "Tracks not found"}</p>
                     <Songs playlist={searchResults.tracks?.items.slice(0, columnCount)}/>
-                    <p className={"flex flex-row space-x-4 text-white text-xl md:text-2xl xl:text-3xl font-bold m-5"}>{searchResults.artists?.items.length!==0 ? "Artists" : "Artists not found"}</p>
+                    <p className={"flex flex-row space-x-4 text-white text-xl md:text-2xl xl:text-3xl font-bold m-5"}>{searchResults.artists?.items.length !== 0 ? "Artists" : "Artists not found"}</p>
                     <div
                         className={"auto-rows-min " + (columnCount !== 0 ? "grid" : "hidden")}
                         style={{gridTemplateColumns: `repeat(${columnCount}, minmax(0, 1fr))`}}>
                         {searchResults.artists?.items.slice(0, columnCount).map(artist =>
                             <Cell key={artist.id + "_cell"} collection={artist}/>)}
                     </div>
-                    <p className={"flex flex-row space-x-4 text-white text-xl md:text-2xl xl:text-3xl font-bold m-5"}>{searchResults.albums?.items.length!==0 ? "Albums" : "Albums not found"}</p>
+                    <p className={"flex flex-row space-x-4 text-white text-xl md:text-2xl xl:text-3xl font-bold m-5"}>{searchResults.albums?.items.length !== 0 ? "Albums" : "Albums not found"}</p>
                     <div
                         className={"auto-rows-min " + (columnCount !== 0 ? "grid" : "hidden")}
                         style={{gridTemplateColumns: `repeat(${columnCount}, minmax(0, 1fr))`}}>
                         {searchResults.albums?.items.slice(0, columnCount).map(album =>
                             <Cell key={album.id + "_cell"} collection={album}/>)}
                     </div>
-                </div> :
-                <p className={"flex flex-row space-x-4 text-white text-xl md:text-2xl xl:text-3xl font-bold m-5"}>Nothing was found</p>}
+                </div>
+                :
+                debouncedValue && (!isFetching)
+                    ?
+                    <p className={"flex flex-row space-x-4 text-white text-xl md:text-2xl xl:text-3xl font-bold m-5"}>Nothing
+                        was found</p>
+                    :
+                    null}
         </div>
     );
 };
